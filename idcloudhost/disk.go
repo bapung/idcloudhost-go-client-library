468bf5e4b2beee4b13e395ff1605dd861fcd1b17
Bagas Purwa <bagaspurwas@gmail.com>
2021-06-07 21:29:19 +0700

    wip: add some func for Vm disk

--- idcloudhost/disk.go
+++ idcloudhost/disk.go
@@ -1,9 +1,12 @@
 package idcloudhost
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 )
 
 type DiskStorage struct {
@@ -21,13 +24,13 @@ type DiskStorage struct {
 	UUID      string   `json:"uuid"`
 }
 
-func (vm *VirtualMachineAPI) AttachDisk(v map[string]interface{}) {
+func (vm *VirtualMachineAPI) AttachDisk(v map[string]interface{}) error {
 	var c HTTPClient = &http.Client{}
 	var disk *DiskStorage
 	diskApiEndpoint := fmt.Sprintf("%s/%s", vm.ApiEndpoint, "disk")
 	data := url.Values{}
-	data.Set("uuid", v["uuid"].(string))
-	data.Set("size_gb", strconv.Iota(v["disk_size"].(int))))
+	data.Set("uuid", v["disk_uuid"].(string))
+	data.Set("size_gb", strconv.Itoa(v["disk_size"].(int)))
 	req, err := http.NewRequest("POST", diskApiEndpoint,
 		strings.NewReader(data.Encode()))
 	if err != nil {
@@ -43,6 +46,68 @@ func (vm *VirtualMachineAPI) AttachDisk(v map[string]interface{}) {
 	if err = checkError(r.StatusCode); err != nil {
 		return err
 	}
-	return json.NewDecoder(r.Body).Decode(&disk)
+	return json.NewDecoder(r.Body).Decode(disk)
 }
 
+func (vm *VirtualMachineAPI) DeleteDisk(v map[string]interface{}) error {
+	var c HTTPClient = &http.Client{}
+	var resp map[string]interface{}
+	diskApiEndpoint := fmt.Sprintf("%s/%s", vm.ApiEndpoint, "disk")
+	data := url.Values{}
+	data.Set("uuid", v["uuid"].(string))
+	data.Set("storage_uuid", v["disk_uuid"].(string))
+	req, err := http.NewRequest("DELETE", diskApiEndpoint,
+		strings.NewReader(data.Encode()))
+	if err != nil {
+		return fmt.Errorf("got error %s", err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("apiKey", vm.AuthToken)
+	r, err := c.Do(req)
+	if err != nil {
+		return fmt.Errorf("got error %s", err.Error())
+	}
+	defer r.Body.Close()
+	if err = checkError(r.StatusCode); err != nil {
+		return err
+	}
+	if err = json.NewDecoder(r.Body).Decode(resp); err != nil {
+		return err
+	}
+	if resp["success"].(bool) != true {
+		return fmt.Errorf("unknown error")
+	}
+	return nil
+}
+
+func (vm *VirtualMachineAPI) ResizeDisk(v map[string]interface{}) error {
+	var c HTTPClient = &http.Client{}
+	var resp map[string]interface{}
+	diskApiEndpoint := fmt.Sprintf("%s/%s", vm.ApiEndpoint, "disk")
+	data := url.Values{}
+	data.Set("uuid", v["uuid"].(string))
+	data.Set("disk_uuid", v["disk_uuid"].(string))
+	data.Set("size_gb", strconv.Itoa(v["disk_size"].(int)))
+	req, err := http.NewRequest("DELETE", diskApiEndpoint,
+		strings.NewReader(data.Encode()))
+	if err != nil {
+		return fmt.Errorf("got error %s", err.Error())
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("apiKey", vm.AuthToken)
+	r, err := c.Do(req)
+	if err != nil {
+		return fmt.Errorf("got error %s", err.Error())
+	}
+	defer r.Body.Close()
+	if err = checkError(r.StatusCode); err != nil {
+		return err
+	}
+	if err = json.NewDecoder(r.Body).Decode(resp); err != nil {
+		return err
+	}
+	if resp["success"].(bool) != true {
+		return fmt.Errorf("unknown error")
+	}
+	return nil
+}
