8c7ccf05c72bd099a466b997a70726ece1beedea
Bagas Purwa <bagaspurwas@gmail.com>
2021-07-16 19:47:36 +0700

    feat: add vpc network API from capturing console requests

---
+++ idcloudhost/vpc_network.go
@@ -0,0 +1,75 @@
+package idcloudhost
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type VPCNetworkAPI struct {
+	c              HTTPClient
+	AuthToken      string
+	Location       string
+	ApiEndpoint    string
+	VPCNetworkList *[]VPCNetwork
+	VPCNetwork     *VPCNetwork
+}
+
+type VPCNetwork struct {
+	CreatedAt      string   `json:"created_at"`
+	UpdatedAt      string   `json:"updated_at"`
+	UUID           string   `json:"uuid"`
+	Name           string   `json:"name"`
+	Type           string   `json:"type"`
+	VlanId         int      `json:"vlan_id"`
+	IsDefault      bool     `json:"is_default"`
+	VMUUIDList     []string `json:"vm_uuids"`
+	Subnet         string   `json:"subnet"`
+	ResourcesCount int      `json:"resources_count"`
+}
+
+func (vpc *VPCNetworkAPI) Init(c HTTPClient, authToken string, location string) error {
+	vpc.c = c
+	vpc.AuthToken = authToken
+	vpc.Location = location
+	vpc.ApiEndpoint = fmt.Sprintf(
+		"https://api.idcloudhost.com/v1/%s/network/networks",
+		vpc.Location,
+	)
+	r, err := http.Get(vpc.ApiEndpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if r.StatusCode == http.StatusNotFound {
+		return fmt.Errorf("location: %s not found", vpc.Location)
+	}
+	return nil
+}
+
+func (vpc *VPCNetworkAPI) Create(name string, billingAccountId int) error {
+	var payloadJSON, err = json.Marshal(
+		&VPCNetwork{
+			Name: name,
+		})
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("POST", vpc.ApiEndpoint,
+		bytes.NewBuffer(payloadJSON))
+	if err != nil {
+		return fmt.Errorf("got error %s", err.Error())
+	}
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	req.Header.Set("apiKey", vpc.AuthToken)
+	r, err := vpc.c.Do(req)
+	if err != nil {
+		return fmt.Errorf("got error %s", err.Error())
+	}
+	defer r.Body.Close()
+	if err = checkError(r.StatusCode); err != nil {
+		return err
+	}
+	return json.NewDecoder(r.Body).Decode(&vpc.VPCNetwork)
+}
